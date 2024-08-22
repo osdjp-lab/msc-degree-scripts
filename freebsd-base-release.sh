b4c5f06553b8898b5d0be9ed2befa62339583a09
Oscar Szumiak <oscarszumiak@gmail.com>
2024-08-23 00:57:16 +0200

    Addition of extracted man page deduplication

--- freebsd-base-release.sh
+++ freebsd-base-release.sh
@@ -206,3 +206,50 @@ rm "$DEST_DIR/man-pkgs"
 
 find "$TMP_DIR" -type d -empty -delete
 
+# Deduplicate extracted man pages based on md5sum
+
+# Temporary files
+
+pkg_tmp="$(mktemp)"
+hash_file_tmp="$(mktemp)"
+ls_tmp="$(mktemp)"
+hash_tmp="$(mktemp)"
+save_tmp="$(mktemp)"
+delete_tmp="$(mktemp)"
+
+find "$MAN_DIR" -mindepth 1 -type d > "$pkg_tmp"
+
+while IFS= read -r pkg; do
+
+    # Writing data to temporary files
+    find "$pkg" -type f | sort > "$ls_tmp"
+    md5sum -- "$pkg"/*  > "$hash_file_tmp"
+    md5sum -- "$pkg"/* | awk '{print $1}' | sort -u > "$hash_tmp"
+
+    while IFS= read -r hash; do
+        grep -m 1 "$hash" "$hash_file_tmp" | awk '{print $2}' >> "$save_tmp"
+    done < "$hash_tmp"
+
+    sort "$save_tmp" -o "$save_tmp"
+    comm -23 "$ls_tmp" "$save_tmp" > "$delete_tmp"
+
+    # Deleting duplicates
+
+    while IFS= read -r file; do
+        rm "$file"
+    done < "$delete_tmp"
+
+    # Clearing temporary files
+
+    true > "$hash_file_tmp"
+    true > "$ls_tmp"
+    true > "$hash_tmp"
+    true > "$save_tmp"
+    true > "$delete_tmp"
+
+done < "$pkg_tmp"
+
+# Cleanup
+
+rm "$pkg_tmp" "$hash_file_tmp" "$ls_tmp" "$hash_tmp" "$save_tmp" "$delete_tmp"
+

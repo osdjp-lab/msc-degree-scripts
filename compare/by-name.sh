85e998ae6108529e91a82221fc343d2d1f966362
Oscar Szumiak <oscarszumiak@gmail.com>
2025-03-11 18:14:25 +0100

    Restructured compare/by-name.sh

--- compare/by-name.sh
+++ compare/by-name.sh
@@ -11,31 +11,16 @@
 
 # Verify number of positional parameters
 
-if [ $# -eq 2 ]; then
-    SRC_DIR="$(realpath "$1")"
-    DEST_DIR="$(realpath "$2")"
+if [ $# -eq 3 ]; then
+    DEBIAN_PKG_NAMES="$(realpath "$1")"
+    FREEBSD_PKG_NAMES="$(realpath "$2")"
+    DEST_DIR="$(realpath "$3")"
 else
-    printf "Usage: %s [SRC_DIR] [DEST_DIR]\n" "$(basename "$0")"
+    printf "Usage: %s [DEBIAN_PKG_NAMES] [FREEBSD_PKG_NAMES] [DEST_DIR]\n" "$(basename "$0")"
     exit
 fi
 
-# Verify values of SRC_DIR and DEST_DIR
-
-if ! [ -d "$SRC_DIR" ]; then
-    printf "Source \"%s\" does not exist or is not a directory\n" "$SRC_DIR"
-    exit
-fi
-
-if ! [ -d "$DEST_DIR" ]; then
-    printf "Destination \"%s\" does not exist or is not a directory\n" "$DEST_DIR"
-    exit
-fi
-
-# Check if package name list files are present and readable
-# in SRC_DIR subdirectories
-
-DEBIAN_PKG_NAMES="$SRC_DIR/debian/names/deduplicated-pkgs"
-FREEBSD_PKG_NAMES="$SRC_DIR/freebsd/base/names/no-man-dbg-dev-lib32"
+# Verify values of DEBIAN_PKG_NAMES, FREEBSD_PKG_NAMES and DEST_DIR
 
 if ! [ -r "$DEBIAN_PKG_NAMES" ]; then
     printf "File \"debian/pkg-names\" in source directory is not readable or does not exist\n"
@@ -47,14 +32,16 @@ if ! [ -r "$FREEBSD_PKG_NAMES" ]; then
     exit
 fi
 
+if ! [ -d "$DEST_DIR" ]; then
+    printf "Destination \"%s\" does not exist or is not a directory\n" "$DEST_DIR"
+    exit
+fi
+
 # Generate initial exact match comparison
 
-TARGET_DIR="$DEST_DIR/by-name"
-mkdir -pv "$TARGET_DIR"
-
-comm -12 "$FREEBSD_PKG_NAMES" "$DEBIAN_PKG_NAMES" > "$TARGET_DIR/common"
-comm -23 "$FREEBSD_PKG_NAMES" "$DEBIAN_PKG_NAMES" > "$TARGET_DIR/freebsd-only"
-comm -13 "$FREEBSD_PKG_NAMES" "$DEBIAN_PKG_NAMES" > "$TARGET_DIR/debian-only"
+comm -12 "$FREEBSD_PKG_NAMES" "$DEBIAN_PKG_NAMES" > "$DEST_DIR/common"
+comm -23 "$FREEBSD_PKG_NAMES" "$DEBIAN_PKG_NAMES" > "$DEST_DIR/freebsd-only"
+comm -13 "$FREEBSD_PKG_NAMES" "$DEBIAN_PKG_NAMES" > "$DEST_DIR/debian-only"
 
 # Generate partial matches
 

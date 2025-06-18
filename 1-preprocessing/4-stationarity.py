cd2b904aad7d7ccf21034f9a069fe0637c166362
Oscar Szumiak <oscarszumiak@gmail.com>
2025-06-18 13:58:47 +0200

    Addition of post differencing stationarity check

--- 1-preprocessing/4-stationarity.py
+++ 1-preprocessing/4-stationarity.py
@@ -11,6 +11,11 @@ output_file = 'data/4-differenced.csv'
 # Load the CSV file
 data = pd.read_csv(input_file, index_col='Date')
 
+print("--------------------------")
+print("ADF for undifferenced data")
+print("--------------------------")
+print("")
+
 # Perform ADF test on each column
 for column in data.columns:
 
@@ -36,6 +41,32 @@ data_diff = data.diff()
 
 # Drop the first row which will have NaN values after differencing
 data_diff = data_diff.dropna()
+
+print("")
+print("------------------------")
+print("ADF for differenced data")
+print("------------------------")
+print("")
+
+# Perform ADF test on each column of the differenced data
+for column in data_diff.columns:
+
+    # Perform ADF test
+    adf_result = adfuller(data_diff[column])
+    
+    # Print the ADF test results
+    print('ADF Statistic: ', adf_result[0])
+    print('p-value: ', adf_result[1])
+    print('Critical Values: ')
+    for key, value in adf_result[4].items():
+        print('\t%s: %.3f' % (key, value))
+    
+    # Determine stationarity
+    if adf_result[1] < 0.05:
+        print(f"{column}: stationary")
+    else:
+        print(f"{column}: non-stationary")
+    print("-----------------------------------")
 
 # Reset the index to include the Date column
 data_diff_reset = data_diff.reset_index()

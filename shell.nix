dad2ffb42e034d68804b0adb770aa3fe540c856c
Oscar Szumiak <oscarszumiak@gmail.com>
2026-02-23 01:07:17 +0100

    Update of enviromnment requirements
    Correction of requirements.txt
    Addition of optuna

--- shell.nix
+++ shell.nix
@@ -1,14 +1,15 @@
 { pkgs ? import <nixpkgs> {} }:
 
 pkgs.mkShell {
-  packages = [
-    (pkgs.python3.withPackages (ps: with ps; [
-      numpy
-      pandas
-      scikit-learn
-      statsmodels
-      matplotlib
-    ]))
-    pkgs.csvkit
-  ];
+    packages = [
+        (pkgs.python3.withPackages (ps: with ps; [
+            numpy
+            pandas
+            scikit-learn
+            optuna
+            statsmodels
+            matplotlib
+        ]))
+        pkgs.csvkit
+    ];
 }

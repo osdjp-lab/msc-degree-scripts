5ebb88a864555f2613266815789d13f0df0e2911
Oscar Szumiak <oscarszumiak@gmail.com>
2026-03-07 09:16:23 +0100

    Addition of cumulative offset mse and hitrate scripts for all models

---
+++ 4-data-presentation/4.3-plot-offset-mse-hitrate/all-rev-avg-hitrate.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python3
+
+import os
+import json
+from pathlib import Path
+
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+######
+# MLP
+######
+
+INPUT_DIR_MLP = Path("../../data/3-training-testing/optuna/mlp/log-normalized/41-USD/")
+hitrate_mlp = pd.read_csv(INPUT_DIR_MLP / 'rev_avg_hitrate.csv').sort_values(by='offset')
+
+######
+# RF
+######
+
+INPUT_DIR_RF = Path("../../data/3-training-testing/optuna/rf/raw/41-USD/")
+hitrate_rf = pd.read_csv(INPUT_DIR_RF / 'rev_avg_hitrate.csv').sort_values(by='offset')
+
+######
+# SVR
+######
+
+INPUT_DIR_SVR = Path("../../data/3-training-testing/optuna/svr/log-diff-standardized/41-USD/")
+hitrate_svr = pd.read_csv(INPUT_DIR_SVR / 'rev_avg_hitrate.csv').sort_values(by='offset')
+
+textsize = 28
+
+plt.figure(figsize=(12, 6))
+
+# Offsets from any model (they should match across models)
+x = hitrate_mlp['offset'].to_numpy()
+width = 0.25  # Width for grouped bars
+
+# Grouped bar chart
+plt.bar(x - width, hitrate_mlp['avg_hitrate'].to_numpy(), width, label="MLP-ln")
+plt.bar(x, hitrate_rf['avg_hitrate'].to_numpy(), width, label="RF-r")
+plt.bar(x + width, hitrate_svr['avg_hitrate'].to_numpy(), width, label="SVR-lds")
+
+plt.xlabel('Offset', fontsize=textsize)
+plt.ylabel('Hitrate', fontsize=textsize)
+
+# Add legend
+plt.legend(fontsize=textsize)
+
+plt.xticks(x)
+
+# Increase tick label size
+plt.xticks(fontsize=textsize-4)
+plt.yticks(fontsize=textsize)
+
+# Show the plot
+plt.show()
+
